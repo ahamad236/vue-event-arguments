d775a31b774d462bbdea0abd4d56ac4e6df8825a
ahamad236 <ahmadshaik4u@gmail.com>
2024-07-17 14:39:50 +0530

    first commit

---
+++ src/components/FirstPageComponent.vue
@@ -0,0 +1,6 @@
+<template>
+    <h1>
+        First Page Component is About Event Arguments
+    </h1>
+    <button @click="$emit('fromChild', 20)">Emit</button>
+</template>
---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Home Component</h1>
+    <FirstPage @fromChild="childFunctionExicuted"></FirstPage>
+</template>
+
+<script setup lang="ts">
+    import FirstPage from "./FirstPageComponent.vue"
+
+    function childFunctionExicuted(value: number){
+        alert(value)
+    }
+</script>
